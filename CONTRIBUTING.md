596e814603d6f2b7d629c475b53bf183c280bc16
Uchit Kumar <uuuchit@gmail.com>
2025-01-13 16:42:26 +0530

    Upgrade to latest Angular and add more features to wizard
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/uuuchit/angular-wizard?shareId=XXXX-XXXX-XXXX-XXXX).

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,2 +1,188 @@
-Please report any bug.
-Contributions to make this great are welcome
+# Contributing to Angular Wizard
+
+Thank you for considering contributing to Angular Wizard! We welcome contributions to make this project even better. Please follow the guidelines below to ensure a smooth contribution process.
+
+## Reporting Bugs
+
+If you find a bug, please report it by opening an issue on the [GitHub repository](https://github.com/uuuchit/angular-wizard/issues). Provide as much detail as possible, including steps to reproduce the issue and any relevant code snippets.
+
+## Feature Requests
+
+If you have an idea for a new feature, please open an issue on the [GitHub repository](https://github.com/uuuchit/angular-wizard/issues) and describe the feature in detail. We welcome suggestions and feedback from the community.
+
+## Contributing Code
+
+To contribute code, follow these steps:
+
+1. Fork the repository on GitHub.
+2. Clone your forked repository to your local machine.
+3. Create a new branch for your feature or bug fix.
+4. Make your changes in the new branch.
+5. Write tests to cover your changes.
+6. Ensure all tests pass.
+7. Commit your changes and push the branch to your forked repository.
+8. Open a pull request on the main repository.
+
+## Coding Guidelines
+
+- Follow the existing code style and conventions.
+- Write clear and concise commit messages.
+- Include comments in your code where necessary to explain complex logic.
+- Write tests to cover your changes and ensure existing tests pass.
+
+## New Features
+
+When contributing new features, please ensure they align with the project's goals and follow the guidelines below:
+
+### Disabling Steps
+
+You can disable steps based on certain conditions by using the `disabled` input property on the `step` component.
+
+```html
+<step [title]="Step one" [disabled]="isStepOneDisabled">
+  <h2>Form will be here</h2>
+</step>
+```
+
+### Custom Step Validation
+
+You can add custom validation for steps by using the `customValidation` input property on the `step` component.
+
+```html
+<step [title]="Step one" [customValidation]="validateStepOne">
+  <h2>Form will be here</h2>
+</step>
+```
+
+### Dynamically Adding or Removing Steps
+
+You can dynamically add or remove steps using the `addStep` and `removeStep` methods on the `wizard` component.
+
+```typescript
+@ViewChild('wizard') wizard: WizardComponent;
+
+addStep() {
+  this.wizard.addStep(newStep);
+}
+
+removeStep(stepIndex: number) {
+  this.wizard.removeStep(stepIndex);
+}
+```
+
+### Nested Wizards
+
+You can create nested wizards by including a `wizard` component inside another `wizard` component.
+
+```html
+<wizard [color]="#ccc">
+  <step [title]="Step one">
+    <h2>Form will be here</h2>
+  </step>
+  <step [title]="Step Two">
+    <wizard [color]="#ccc">
+      <step [title]="Nested Step one">
+        <h2>Nested form will be here</h2>
+      </step>
+      <step [title]="Nested Step Two">
+        <h2>Second part of nested form will be here</h2>
+      </step>
+    </wizard>
+  </step>
+</wizard>
+```
+
+### Customizing Navigation Buttons
+
+You can customize the navigation buttons by using the `nextButtonText` and `backButtonText` input properties on the `wizard` component.
+
+```html
+<wizard [color]="#ccc" [nextButtonText]="'Next Step'" [backButtonText]="'Previous Step'">
+  <step [title]="Step one">
+    <h2>Form will be here</h2>
+  </step>
+  <step [title]="Step Two">
+    <h2>Second part of Form will be here</h2>
+  </step>
+</wizard>
+```
+
+### Step Transition Animations
+
+You can add different step transition animations by using the `transitionAnimation` input property on the `wizard` component.
+
+```html
+<wizard [color]="#ccc" [transitionAnimation]="'fade'">
+  <step [title]="Step one">
+    <h2>Form will be here</h2>
+  </step>
+  <step [title]="Step Two">
+    <h2>Second part of Form will be here</h2>
+  </step>
+</wizard>
+```
+
+### Saving and Restoring Wizard State
+
+You can save and restore the wizard state using the `saveState` and `restoreState` methods on the `wizard` component.
+
+```typescript
+@ViewChild('wizard') wizard: WizardComponent;
+
+saveWizardState() {
+  const state = this.wizard.saveState();
+  localStorage.setItem('wizardState', JSON.stringify(state));
+}
+
+restoreWizardState() {
+  const state = JSON.parse(localStorage.getItem('wizardState'));
+  this.wizard.restoreState(state);
+}
+```
+
+### Keyboard Navigation
+
+You can navigate between steps using the keyboard (e.g., arrow keys) by enabling keyboard navigation on the `wizard` component.
+
+```html
+<wizard [color]="#ccc" [keyboardNavigation]="true">
+  <step [title]="Step one">
+    <h2>Form will be here</h2>
+  </step>
+  <step [title]="Step Two">
+    <h2>Second part of Form will be here</h2>
+  </step>
+</wizard>
+```
+
+### Progress Bar
+
+You can display a progress bar indicating the completion status of the wizard by enabling the progress bar on the `wizard` component.
+
+```html
+<wizard [color]="#ccc" [showProgressBar]="true">
+  <step [title]="Step one">
+    <h2>Form will be here</h2>
+  </step>
+  <step [title]="Step Two">
+    <h2>Second part of Form will be here</h2>
+  </step>
+</wizard>
+```
+
+### Internationalization and Localization
+
+You can add support for internationalization and localization by providing translations for the wizard component.
+
+```typescript
+import { TranslateService } from '@ngx-translate/core';
+
+constructor(private translate: TranslateService) {
+  translate.setDefaultLang('en');
+  translate.use('en');
+}
+```
+
+## Thank You
+
+Thank you for your contributions! Together, we can make Angular Wizard even better.
--- src/wizard.ts
+++ src/wizard.ts
@@ -1,149 +1,217 @@
-import { Component, ContentChildren, QueryList, AfterContentInit,EventEmitter, Input, Output} from '@angular/core';
+import { Component, ContentChildren, QueryList, AfterContentInit, EventEmitter, Input, Output, HostListener } from '@angular/core';
 import { StepComponent } from './step';
 
-
 @Component({
-    selector: 'wizard',
-    template: `
-        <div class="uk-wizard">
-            <div class="uk-wizard-nav">
-                <ul>
-                  <li *ngFor="let item of items" (click)="navClicked(item.id)"
-                   [style.width.%]="navWidth">
-                   <div class="uk-li" [style.border-bottom-color]="color">
-                    <span *ngIf="item.step.title">{{item.step.title}}</span>
-                    <span *ngIf="!item.step.title">Step {{item.id +1}}</span>
-                   </div>
-                   <div class="uk-overlay" [class.active]="item.id===currentStepIndex">
-                   </div>
-                  </li>
-                </ul>
+  selector: 'wizard',
+  template: `
+    <div class="uk-wizard">
+      <div class="uk-wizard-nav">
+        <ul>
+          <li *ngFor="let item of items" (click)="navClicked(item.id)" [style.width.%]="navWidth">
+            <div class="uk-li" [style.border-bottom-color]="color">
+              <span *ngIf="item.step.title">{{item.step.title}}</span>
+              <span *ngIf="!item.step.title">Step {{item.id + 1}}</span>
             </div>
-            <div class="uk-wizard-steps">
-              <ng-content></ng-content>
-            </div>
-        </div>
-    `,
-    styles: [`
-      .uk-wizard{
-        border: 1px solid #ccc;
-        border-radius: 2px;
-      }
-      .uk-wizard-nav{
-        width: 100%;
-      }
-      .uk-wizard-steps{
-        width: 100%
-      }
-      ul{
-        padding: 0px;
-        margin: 0px;
-      }
-      li{
-        display: inline-block;
-        min-width: 100px;
-        overflow: hidden;
-        position: relative;
-      }
-      .uk-li{
-        width: 100%;
-        border-bottom: 5px solid;
-        padding: 10px 0px 5px 0px;
-        text-align: center;
-      }
-      li:hover{
-        cursor:pointer;
-      }
-      .uk-overlay{
-        position: absolute;
-        bottom: 0px;
-        right: 0px;
-        width: 100%;
-        height: 5px;
-        opacity: .8;
-        background-color: #ccc;
-      }
-      .uk-overlay.active{
-        opacity: 0.0;
-      }
-      :host >>> #uk-back, :host >>> #uk-next{
-        min-width: 50px;
-        padding: 5px;
-      }
-
-    `]
+            <div class="uk-overlay" [class.active]="item.id === currentStepIndex"></div>
+          </li>
+        </ul>
+      </div>
+      <div class="uk-wizard-steps">
+        <ng-content></ng-content>
+      </div>
+      <div *ngIf="showProgressBar" class="progress-bar">
+        <div class="progress" [style.width.%]="progress"></div>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .uk-wizard {
+      border: 1px solid #ccc;
+      border-radius: 2px;
+    }
+    .uk-wizard-nav {
+      width: 100%;
+    }
+    .uk-wizard-steps {
+      width: 100%;
+    }
+    ul {
+      padding: 0px;
+      margin: 0px;
+    }
+    li {
+      display: inline-block;
+      min-width: 100px;
+      overflow: hidden;
+      position: relative;
+    }
+    .uk-li {
+      width: 100%;
+      border-bottom: 5px solid;
+      padding: 10px 0px 5px 0px;
+      text-align: center;
+    }
+    li:hover {
+      cursor: pointer;
+    }
+    .uk-overlay {
+      position: absolute;
+      bottom: 0px;
+      right: 0px;
+      width: 100%;
+      height: 5px;
+      opacity: .8;
+      background-color: #ccc;
+    }
+    .uk-overlay.active {
+      opacity: 0.0;
+    }
+    :host >>> #uk-back, :host >>> #uk-next {
+      min-width: 50px;
+      padding: 5px;
+    }
+    .progress-bar {
+      width: 100%;
+      height: 10px;
+      background-color: #f3f3f3;
+      border-radius: 5px;
+      overflow: hidden;
+      margin-top: 10px;
+    }
+    .progress {
+      height: 100%;
+      background-color: #4caf50;
+      transition: width 0.3s;
+    }
+  `]
 })
-
-export class WizardComponent implements AfterContentInit{
-   items: any;
-   currentStepIndex: number = 0;
-   currentStep: string;
-   navWidth: number;
-   @Input() color: string = 'blue';
-   @ContentChildren(StepComponent) steps: QueryList<StepComponent>;
+export class WizardComponent implements AfterContentInit {
+  items: any;
+  currentStepIndex: number = 0;
+  currentStep: string;
+  navWidth: number;
+  @Input() color: string = 'blue';
+  @Input() nextButtonText?: string;
+  @Input() backButtonText?: string;
+  @Input() transitionAnimation?: string;
+  @Input() showProgressBar: boolean = false;
+  @Input() keyboardNavigation: boolean = false;
+  @Input() translations: any;
+  @ContentChildren(StepComponent) steps: QueryList<StepComponent>;
   @Output() onFinish = new EventEmitter<this>();
-   navClicked(id: number) {
+  progress: number = 0;
+
+  navClicked(id: number) {
+    if (this.items[id].step.disabled) {
+      return;
+    }
     this.items[this.currentStepIndex].step.activeStep = false;
     this.currentStepIndex = id;
     this.currentStep = this.items[id];
     this.items[this.currentStepIndex].step.activeStep = true;
-    console.log(this.currentStep);
-   }
-    goto(id: number) {
-      if ( id >= this.items.length){
-         this.onFinish.emit(this.items);
-      }
-      else if(id < 0)
-        {
-        this.goto(0)
-      }
-      else{
-        this.items[this.currentStepIndex].step.activeStep = false;
-        this.currentStepIndex = id;
-        this.currentStep = this.items[id];
-        this.items[this.currentStepIndex].step.activeStep = true;
-        console.log(this.currentStep);
-      }
+    this.updateProgress();
+  }
 
-   }
-   ngAfterContentInit() {
-     let i = 0;
-    this.items = this.steps.map((r) => { return {step: r, id: i++}; });
+  goto(id: number) {
+    if (id >= this.items.length) {
+      this.onFinish.emit(this.items);
+    } else if (id < 0) {
+      this.goto(0);
+    } else {
+      if (this.items[id].step.disabled) {
+        return;
+      }
+      this.items[this.currentStepIndex].step.activeStep = false;
+      this.currentStepIndex = id;
+      this.currentStep = this.items[id];
+      this.items[this.currentStepIndex].step.activeStep = true;
+      this.updateProgress();
+    }
+  }
+
+  addStep(step: StepComponent) {
+    this.items.push({ step: step, id: this.items.length });
+    this.navWidth = 100 / this.items.length;
+  }
+
+  removeStep(stepIndex: number) {
+    this.items.splice(stepIndex, 1);
+    this.navWidth = 100 / this.items.length;
+  }
+
+  saveState() {
+    return {
+      currentStepIndex: this.currentStepIndex,
+      steps: this.items.map((item: any) => ({
+        title: item.step.title,
+        activeStep: item.step.activeStep,
+        firstStep: item.step.firstStep,
+        lastStep: item.step.lastStep,
+        shouldEnter: item.step.shouldEnter,
+        disabled: item.step.disabled
+      }))
+    };
+  }
+
+  restoreState(state: any) {
+    this.currentStepIndex = state.currentStepIndex;
+    this.items = state.steps.map((step: any, index: number) => ({
+      step: { ...step, id: index },
+      id: index
+    }));
+    this.navWidth = 100 / this.items.length;
+    this.items[this.currentStepIndex].step.activeStep = true;
+    this.updateProgress();
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (!this.keyboardNavigation) {
+      return;
+    }
+    if (event.key === 'ArrowRight') {
+      this.goto(this.currentStepIndex + 1);
+    } else if (event.key === 'ArrowLeft') {
+      this.goto(this.currentStepIndex - 1);
+    }
+  }
+
+  updateProgress() {
+    this.progress = ((this.currentStepIndex + 1) / this.items.length) * 100;
+  }
+
+  ngAfterContentInit() {
+    let i = 0;
+    this.items = this.steps.map((r) => ({ step: r, id: i++ }));
     this.items[this.currentStepIndex].step.activeStep = true; // setting default step as active step.
     this.items[0].step.firstStep = true;
     this.items[this.items.length - 1].step.lastStep = true;
-    this.navWidth = 100/ this.items.length;
+    this.navWidth = 100 / this.items.length;
+    this.updateProgress();
     this.steps.forEach((r) => {
       r.onBack.subscribe((title: StepComponent) => {
-        if(this.currentStepIndex === 0){
+        if (this.currentStepIndex === 0) {
           console.log("you cannot go back from here!!");
-        }
-        else{
+        } else {
           this.items[this.currentStepIndex].step.activeStep = false;
           this.currentStepIndex -= 1;
           this.currentStep = this.items[this.currentStepIndex];
           this.items[this.currentStepIndex].step.activeStep = true;
-          console.log(this.currentStep);
+          this.updateProgress();
         }
-
-
       });
-      r.onNext.subscribe((title : StepComponent) => {
-        if(this.currentStepIndex == this.items.length - 1){
-          //TODO call on finish
+      r.onNext.subscribe((title: StepComponent) => {
+        if (this.currentStepIndex == this.items.length - 1) {
           this.onFinish.emit(this.items);
           console.log("Finished");
-        }
-        else{
+        } else {
           this.items[this.currentStepIndex].step.activeStep = false;
           this.currentStepIndex += 1;
           this.currentStep = this.items[this.currentStepIndex];
           this.items[this.currentStepIndex].step.activeStep = true;
-          console.log(this.currentStep);
+          this.updateProgress();
         }
       });
-  } );
-    console.log(this.steps);
-    // setTimeout((r)=>{this.items[0].step.onNext.emit(this.items[0]); console.log("")}, 10000);
-   }
+    });
+  }
 }
